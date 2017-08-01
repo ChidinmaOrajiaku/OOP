9e36f3e00ae8989ab35fb86624211cdf4f7bea81
Chidinma Orajiaku <corajiaku96@gmail.com>
2017-08-01 17:21:29 +0100

    Dice test cases

---
+++ tests/test.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert
+let FairDie = require('../src/main.js').FairDie;
+
+describe("FairDie", () => {
+
+   describe("maxNum",() => {
+   	beforeEach(() => {
+        FairDie = new FairDie(6);
+   	}),
+     it("returns",() =>{
+        assert.equal(FairDie.max,6);
+     }); 
+   });
+
+   describe ("die1 and die2",() => {
+   	 it("returns",() => {
+      assert.equal(typeof(FairDie.die1() && FairDie.die2()), "number")
+   });
+   	});
+
+   describe("total",() => {
+       it ("returns",() => {
+          assert.equal(FairDie.total().length, 2)
+       });
+   });
+
+
+    describe("thrower",() => {
+       it ("returns",() => {
+          assert.equal(FairDie.thrower(), "move" + " " +(FairDie.total()[0] + FairDie.total()[1]))
+       });
+
+});
+});
